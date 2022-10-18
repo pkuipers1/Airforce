2725867cc87926b68aed648a69ba9984b1d19c26
bas-boop <dereusbas461@gmail.com>
2022-10-18 22:34:27 +0200

    Perfomence issue fixed

--- Assets/Scripts/Framework/Camera/CameraMovement.cs
+++ Assets/Scripts/Framework/Camera/CameraMovement.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     [SerializeField] private float speedDivider;
-    private GameObject _player;
-
-    private void Awake()
-    {
-        _player = GameObject.Find("TestObject");// TODO: dit moet veranderd worden naar de goed speler of speler tag
-    }
 
     private void FixedUpdate()
     {
-        var playerPos = _player.transform.position;
+        var playerPos = player.transform.position;
         var wantedPosition = new Vector3(playerPos.x / speedDivider, playerPos.y / speedDivider, -10);
         transform.position = wantedPosition;
     }
