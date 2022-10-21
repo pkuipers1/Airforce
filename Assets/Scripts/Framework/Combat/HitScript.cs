25d872a0e4c8393f916b9a028e62fbd7a78395db
Patricia Kuipers <28625@ma-web.nl>
2022-10-21 17:42:48 +0200

    player and enemy can get hit
    Co-Authored-By: bas-boop <70578065+bas-boop@users.noreply.github.com>

--- Assets/Scripts/Framework/Combat/HitScript.cs
+++ Assets/Scripts/Framework/Combat/HitScript.cs
@@ -1,17 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HitScript : MonoBehaviour
-{
-    
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
+{ 
+    private void OnTriggerEnter2D(Collider2D col)
+    { 
+        if (col.gameObject.HasTag("PlayerBullet") && gameObject.HasTag("Enemy"))
+        {
+            Debug.Log("Enemy hit!");
+        }
+        if (col.gameObject.HasTag("EnemyBullet") && gameObject.HasTag("Player"))
+        {
+            Debug.Log("Player hit!");
+        }
     }
 }
